2e791854357c6ea6a219d65ede5185a3e027d706
kovacsaronlaszlo <k.aron.laszlo@gmail.com>
2024-05-29 13:20:18 +0200

    proper unit test structure with it without comments

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -2,14 +2,12 @@ import { toUpperCase } from "../app/Utils";
 
 describe("Utils test suite", () => {
   it("should return Uppercase", () => {
-    //arrange:
+    
     const sut = toUpperCase;
     const expected = "ABC";
 
-    // act:
     const actual = sut("abc");
 
-    //assert:
     expect(actual).toBe(expected);
   });
 });
