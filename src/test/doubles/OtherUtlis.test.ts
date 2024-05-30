3e37931e28e721d79e07773469ec078416de2d88
kovacsaronlaszlo <k.aron.laszlo@gmail.com>
2024-05-30 11:19:38 +0200

    add 2 different mocks

--- src/test/doubles/OtherUtlis.test.ts
+++ src/test/doubles/OtherUtlis.test.ts
@@ -4,6 +4,58 @@ import {
 } from "../../app/doubles/OtherUtlis";
 
 describe("OtherUtils test suite", () => {
+  describe("Tracking callback with Jest mocks", () => {
+    const callbackMock = jest.fn();
+
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it("calls callback for invalid argument - track calls", () => {
+      const actual = toUpperCaseWithCallback("", callbackMock);
+      expect(actual).toBeUndefined();
+      expect(callbackMock).toBeCalledWith("Invalid argument");
+      expect(callbackMock).toBeCalledTimes(1);
+    });
+
+    it("calls callback for valid argument - track calls", () => {
+      const actual = toUpperCaseWithCallback("abc", callbackMock);
+      expect(actual).toBe("ABC");
+      expect(callbackMock).toBeCalledWith("called function with abc");
+      expect(callbackMock).toBeCalledTimes(1);
+    });
+  });
+
+  describe("Tracking clabbacks", () => {
+    let cbArg = [];
+    let timesCalled = 0;
+
+    function callbackMock(arg: string) {
+      cbArg.push(arg);
+      timesCalled++;
+    }
+
+    afterEach(() => {
+      // clearing tracking fields
+      cbArg = [];
+      timesCalled = 0;
+    });
+
+    it("calls callback for invalid argument - track calls", () => {
+      const actual = toUpperCaseWithCallback("", callbackMock);
+      expect(actual).toBeUndefined();
+      expect(cbArg).toContain("Invalid argument");
+      expect(timesCalled).toBe(1);
+    });
+
+    it("calls callback for valid argument - track calls", () => {
+      const actual = toUpperCaseWithCallback("abc", callbackMock);
+      expect(actual).toBe("ABC");
+      expect(cbArg).toContain("called function with abc");
+      expect(timesCalled).toBe(1);
+    });
+  });
+
   it("to uppercase - call callback for invalid argument", () => {
     const actual = toUpperCaseWithCallback("", () => {}); // () => {} => this is the fake
     expect(actual).toBeUndefined();
